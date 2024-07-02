7088d169c2e6686e07ebd5ab2e6acc65d9e9b372
Nicholas Kearney <nkearney757@gmail.com>
2024-07-02 11:24:53 +0200

    Add stamp

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -8,11 +8,16 @@ type CellProps = {
 const Cell = ({ text, index, handleClick, selected }: CellProps) => {
   return (
     <div
-      className={`transition-colors border-2 border-black flex flex-col justify-center items-center aspect-square border-box lg:hover:bg-gray-500 lg:relative lg:p-4 ${
-        selected ? "bg-gray-500" : ""
-      }`}
+      className="transition-colors border-2 border-black flex flex-col justify-center items-center aspect-square border-box relative lg:hover:bg-gray-500 lg:p-4"
       onClick={() => handleClick(index)}
     >
+      {selected && (
+        <img
+          src="/stamp.svg"
+          className="absolute inset-0 w-full h-full"
+          alt="Bingo Stamp"
+        />
+      )}
       <span className="font-bold lg:absolute lg:top-2 lg:left-2">{index}</span>
       <span className="hidden lg:block">{text}</span>
     </div>
